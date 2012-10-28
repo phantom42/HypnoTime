47b8ae84cdb617aee8b302c722053f33d6651bd2
Joe Coleman <phantom42@gmail.com>
2012-10-28 15:02:01 -0400

    Initial Commit

---
+++ HypnoTime/hypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  hypnosisterAppDelegate.h
+//  HypnoTime
+//
+//  Created by Joe Coleman on 10/28/12.
+//  Copyright (c) 2012 Joe Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface hypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by Joe Coleman on 10/28/12.
+//  Copyright (c) 2012 Joe Coleman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "hypnosisterAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([hypnosisterAppDelegate class]));
+    }
+}
