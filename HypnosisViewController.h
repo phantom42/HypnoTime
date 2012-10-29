4a5560a8084deb8a53920d9295f05eb9e13c92f9
Joe Coleman <phantom42@gmail.com>
2012-10-29 15:00:47 -0400

    silver challenge complete
    added segmented control to manually select r/g/b color for hypnosis
    circles

--- HypnosisViewController.h
+++ HypnosisViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "HypnosisView.h"
 
 @interface HypnosisViewController : UIViewController
-
+{
+    // set these as instance vars so we can access them easier from methods
+    HypnosisView *v ; // kept var name from tutorial even though it's not descriptive enough
+    
+}
+@property(nonatomic) NSInteger selectedSegmentIndex ;
+- (void)segmentChanged:(id)sender ;
 @end
--- HypnosisViewController.m
+++ HypnosisViewController.m
@@ -15,11 +15,45 @@
 {
     // create a view
     CGRect frame = [[UIScreen mainScreen] bounds] ;
-    HypnosisView *v = [[HypnosisView alloc] initWithFrame:frame] ;
+    v = [[HypnosisView alloc] initWithFrame:frame] ;
     
     // set it as *the* view of this view controller
     [self setView: v] ;
+    
+    // create a UISegmentedControl. requires an array of items for button labels
+    // we'll also dynamically size/position it towards the bottom. 
+    NSArray *controlItems = [NSArray arrayWithObjects:@"Red",@"Green",@"Blue", nil];
+    CGFloat segmentHeight = 50 ;
+    CGFloat segmentTop = frame.size.height - 125 ;
+    CGFloat segmentLeft = 25 ;
+    CGFloat segmentWidth = frame.size.width - 50 ;
+    CGRect segmentRect = CGRectMake(segmentLeft, segmentTop, segmentWidth, segmentHeight) ;
+    UISegmentedControl *segmentControl = [[UISegmentedControl alloc] initWithItems:controlItems] ;
+    [segmentControl setFrame:segmentRect] ;
+    [segmentControl addTarget:self
+                       action:@selector(segmentChanged:)
+             forControlEvents:UIControlEventValueChanged] ;
+    
+    // now add the segment control to the view as a subview
+    [v addSubview:segmentControl] ;
 }
+- (void)segmentChanged:(id)sender
+{
+    NSInteger segmentSelected = [sender selectedSegmentIndex];
+    switch (segmentSelected){
+        case 0: // red
+            [v setCircleColor:[UIColor redColor]] ;
+            break ;
+        case 1: // green
+            [v setCircleColor:[UIColor greenColor]] ;
+            break ;
+        case 2: // blue
+            [v setCircleColor:[UIColor blueColor]] ;
+            break ;
+    }
+    
+}
+
 - (id)initWithNibName:(NSString *)nibName bundle:(NSBundle *)bundle
 {
     // call the superclass's designated initializer
