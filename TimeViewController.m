14ba7fa4ff059853a278c7583d5179bd02033057
Joe Coleman <phantom42@gmail.com>
2012-10-29 10:22:00 -0400

    basic wiring and functionality of 'what time is it' button

--- TimeViewController.m
+++ TimeViewController.m
@@ -9,5 +9,14 @@
 #import "TimeViewController.h"
 
 @implementation TimeViewController
+- (IBAction)showCurrentTime:(id)sender
+{
+    NSDate *now = [NSDate date] ;
+    
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init] ;
+    [formatter setTimeStyle:NSDateFormatterMediumStyle] ;
+    
+    [timeLabel setText:[formatter stringFromDate:now]] ;
+}
 
 @end
